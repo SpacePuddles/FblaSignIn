7a87714f0581a73c3bed8ed013e120a5def653f9
Caleb Williamson <afreakinbanana@outlook.com>
2016-01-13 09:17:12 -0800

    Update StudentView.cs
    Added some stuff for a populate method

--- FBLA/FBLA/StudentView.cs
+++ FBLA/FBLA/StudentView.cs
@@ -21,5 +21,35 @@ namespace FBLA {
         private void metroButton3_Click(object sender, EventArgs e) {
             this.Close();
         }
+        
+        // We could use this to populate a database looking list. If we need it anyway.
+        private void populate() {
+            SqlCommand cmd;
+            SqlReader reader;
+            string sql;
+            
+            using (SqlConnection con = new SqlConnection()) {
+                string id, fname, lname, grade, studentid, fblaid, teamstring, eventstring;
+                sql = "SELECT * FROM DBO.Users;";
+                cmd = new SqlCommand(sql);
+                
+                con.ConnectionString = "";
+                con.Open();
+                
+                reader = cmd.ExecuteReader();
+                while(reader.Read()) {
+                    id = reader["ID"].ToString();
+                    fname = reader["[First Name]"].ToString();
+                    lname = reader["[Last Name]"].ToString();
+                    grade = reader["Grade"].ToString();
+                    studentid = reader["[Student ID]"].ToString();
+                    fblaid = reader["[FBLA ID]"].ToString();
+                    teamstring = reader["[Team IDs]"].ToString();
+                    eventstring = reader["[Event IDs]"].ToString();
+                }
+                
+                //Close con and make student later
+            }
+        }
     }
 }
