d443381002233ada0a1c7e81d8d53398d9919900
SpacePuddles <t.j.c.hickman@gmail.com>
2016-02-04 11:42:21 -0800

    Update StudentMain.cs

--- FBLA/FBLA/StudentMain.cs
+++ FBLA/FBLA/StudentMain.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
-namespace FBLA
+namespace FBLASignIn
 {
     public partial class StudentMain : Form
     {
+        int Events = 0; 
+        String Studentid; 
         public StudentMain(String userid)
         {
-            
-        }
+            this.CenterToScreen(); 
+            InitializeComponent();
+            Studentid = userid; 
+        }        
 
         private void StudentMain_Load(object sender, EventArgs e)
         {
+            //loads information into SQL database
 
+            //this.studentsTableAdapter.Fill(this.studentDataDataSet.Students);
+            //button1.Enabled = false;
+            //studentsBindingSource.Filter = "id = " + Studentid;
+        }
+
+        private void Logout_Click(object sender, EventArgs e)
+        {
+            //matches button3
+            Studentid = null;
+            this.Close();
+            new LogIn().Show(); 
+        }
+
+        private void addevent_Click(object sender, EventArgs e)
+        {
+            //add new event 
+            //matches button1 
+            this.Hide();
+            new AddEvent(Studentid).Show(); 
+        }
+
+        private void editevent_Click(object sender, EventArgs e)
+        {
+            //edit event
+            //matches button2
+            if (Events != 0)
+            {
+                this.Hide(); 
+                new EditEvent().Show();
+            }
+            else
+            {
+                MessageBox.Show("You have no Events yet! Add Event first");
+            }
+        }
+
+        private void MessageBoxIcon(string p)
+        {
+            throw new NotImplementedException();
         }
     }
 }
+
